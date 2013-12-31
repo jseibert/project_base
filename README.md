6a9465348e7dd1c7e20e80a7364066357eb1e3e2
Jeff Seibert <jeff@crashlytics.com>
2013-12-30 21:43:26 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-project_base
+Project Base
 ============
 
-A launching point for a new web project...
+A launching point for a new web project... this sets up:
+- Vagrant running Ubuntu Precise AMD64
+- Chef Solo and Librarian
+- rbenv and Roby 2.0.0p247
+- Nginx
---
+++ cookbooks/base/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: base
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "rbenv::default"
+include_recipe "rbenv::ruby_build"
+
+rbenv_ruby "2.0.0-p247" do
+  global true
+end
+
+rbenv_gem "bundler" do
+  ruby_version "2.0.0-p247"
+end
